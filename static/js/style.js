208d4095515ced427d91ea332858f6bd6a602dd4
vedran01 <vedran2310@hotmail.com>
2017-08-16 21:49:01 +0200

    admin panel fakulteti

---
+++ static/js/style.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+   $("#fakulteti").click(function(){
+        $(".content").toggle();
+           }); 
+    
+    $("#btn_dodaj_fakultet").click(function(){
+        $("#form_dodaj_fakultet").toggle();
+    });
+});
